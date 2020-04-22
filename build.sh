90e6bd401753892f6c64a33aa06251b4b76f94eb
ANDROID2468 <39718118+ANDROID2468@users.noreply.github.com>
2020-04-22 12:54:57 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ echo "removing old build logs"
 rm -f log_vs986_usu.txt
 rm -f log_h815.txt
 rm -f log_h815_usu.txt
-rm -f log_h811_usu.txt
+rm -f log_h811.txt
 rm -f log_h812_usu.txt
 rm -f log_h810_usu.txt
 
@@ -23,7 +23,7 @@ breakfast potato_vs986_usu-userdebug | tee -a log_vs986_usu.txt
 brunch potato_vs986_usu-userdebug | tee -a log_vs986_usu.txt
 echo "md5 for venus.mbn" | tee -a log_vs986.txt
 md5sum out/target/product/vs986_usu/system/etc/firmware/venus.mbn | tee -a log_vs986_usu.txt
-echo "The md5sum should be 78e5cf520d0de4a413ef1cfa7bbbe713" | tee -a log_vs986_usu.txt
+echo "78e5cf520d0de4a413ef1cfa7bbbe713 should be the md5" | tee -a log_vs986_usu.txt
 
 # h810_usu
 echo "starting build for h810_usu" | tee -a log_h810_usu.txt
@@ -31,15 +31,15 @@ breakfast potato_h810_usu-userdebug | tee -a log_h810_usu.txt
 brunch potato_h810_usu-userdebug | tee -a log_h810_usu.txt
 echo "md5 for venus.mbn" | tee -a log_h810_usu.txt
 md5sum out/target/product/h810_usu/system/etc/firmware/venus.mbn | tee -a log_h810_usu.txt
-echo "The md5sum should be 78e5cf520d0de4a413ef1cfa7bbbe713" | tee -a log_h810_usu.txt
+echo "78e5cf520d0de4a413ef1cfa7bbbe713 should be the md5" | tee -a log_h810_usu.txt
 
-# h811_usu
-echo "starting build for h811" | tee -a log_h811_usu.txt
-breakfast potato_h811_usu-userdebug | tee -a log_h811_usu.txt
-brunch potato_h811_usu-userdebug | tee -a log_h811_usu.txt
-echo "md5 for venus.mbn" | tee -a log_h811_usu.txt
-md5sum out/target/product/h811_usu/system/etc/firmware/venus.mbn | tee -a log_h811_usu.txt
-echo "The md5sum should be 78e5cf520d0de4a413ef1cfa7bbbe713" | tee -a log_h811_usu.txt
+# h811
+echo "starting build for h811" | tee -a log_h811.txt
+breakfast potato_h811-userdebug | tee -a log_h811.txt
+brunch potato_h811-userdebug | tee -a log_h811.txt
+echo "md5 for venus.mbn" | tee -a log_h811.txt
+md5sum out/target/product/h811/system/etc/firmware/venus.mbn | tee -a log_h811.txt
+echo "78e5cf520d0de4a413ef1cfa7bbbe713 should be the md5" | tee -a log_h811.txt
 
 # sleep for 30 min
 echo "letting computer cool down for 30 mins"
@@ -51,7 +51,7 @@ breakfast potato_h812_usu-userdebug | tee -a log_h812_usu.txt
 brunch potato_h812_usu-userdebug | tee -a log_h812_usu.txt
 echo "md5 for venus.mbn" | tee -a log_h812_usu.txt
 md5sum out/target/product/h812_usu/system/etc/firmware/venus.mbn | tee -a log_h812_usu.txt
-echo "The md5sum should be 78e5cf520d0de4a413ef1cfa7bbbe713" | tee -a log_h812_usu.txt
+echo "78e5cf520d0de4a413ef1cfa7bbbe713 should be the md5" | tee -a log_h812_usu.txt
 
 # h815
 echo "starting build for h815" | tee -a log_h815.txt
@@ -59,7 +59,7 @@ breakfast potato_h815-userdebug | tee -a log_h815.txt
 brunch potato_h815-userdebug | tee -a log_h815.txt
 echo "md5 for venus.mbn" | tee -a log_h815.txt
 md5sum out/target/product/h815/system/etc/firmware/venus.mbn | tee -a log_h815.txt
-echo "The md5 shuld be d1f6fe863643b1e8d1e597762474928c" | tee -a log_h815.txt
+echo "d1f6fe863643b1e8d1e597762474928c should be the md5" | tee -a log_h815.txt
 
 # h815_usu
 echo "starting build for h815_usu" | tee -a log_h815_usu.txt
@@ -67,6 +67,6 @@ breakfast potato_h815_usu-userdebug | tee -a log_h815_usu.txt
 brunch potato_h815_usu-userdebug | tee -a log_h815_usu.txt
 echo "md5 for venus.mbn" | tee -a log_h815_usu.txt
 md5sum out/target/product/h815_usu/system/etc/firmware/venus.mbn | tee -a log_h815_usu.txt
-echo "The md5 shuld be d1f6fe863643b1e8d1e597762474928c" | tee -a log_h815_usu.txt
+echo "d1f6fe863643b1e8d1e597762474928c should be the md5" | tee -a log_h815_usu.txt
 
 echo "Done!"
